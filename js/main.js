2371e3ef9fb522e8f695e395412a6f4354b3e816
mariajose16 <60947581+mariajose16@users.noreply.github.com>
2020-05-26 16:50:41 -0500

    26 mayo

---
+++ js/main.js
@@ -0,0 +1,9 @@
+const listaProyectos = document.querySelector(".menu-top");
+
+const lesgaiCinemad = listaProyectos.querySelector(".link");
+
+lesgaiCinemad.addEventListener("click", logOutput);
+
+function logOutput() {
+  console.log(lesgaiCinemad);
+}
